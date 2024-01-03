2757986fc557400a279e9dd29703ef02c9cb4a2f
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2024-01-03 16:22:23 -0300

    test: initial movie service test

--- src/test/java/com/devsuperior/dsmovie/services/MovieServiceTests.java
+++ src/test/java/com/devsuperior/dsmovie/services/MovieServiceTests.java
@@ -1,9 +1,31 @@
 package com.devsuperior.dsmovie.services;
 
+import com.devsuperior.dsmovie.dto.MovieDTO;
+import com.devsuperior.dsmovie.entities.MovieEntity;
+import com.devsuperior.dsmovie.repositories.MovieRepository;
+import com.devsuperior.dsmovie.services.exceptions.DatabaseException;
+import com.devsuperior.dsmovie.services.exceptions.ResourceNotFoundException;
+import com.devsuperior.dsmovie.tests.MovieFactory;
+import jakarta.persistence.EntityNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(SpringExtension.class)
 public class MovieServiceTests {
@@ -11,39 +33,83 @@ public class MovieServiceTests {
 	@InjectMocks
 	private MovieService service;
 	
+
+	@Mock
+	private MovieRepository repository;
+
+	private MovieEntity movieEntity;
+	private MovieDTO movieDTO;
+	private String title;
+	private Pageable pageable;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		movieEntity = MovieFactory.createMovieEntity();
+		movieDTO = MovieFactory.createMovieDTO();
+		title = "Test Movie";
+		pageable = PageRequest.of(1, 6, Sort.by("title"));
+
+		// Expected result in find all
+		when(repository.searchByTitle(title, pageable)).thenReturn(new PageImpl<>(List.of(movieEntity)));
+
+		// Expected result in find by id
+		when(repository.findById(1L)).thenReturn(Optional.of(movieEntity));
+
+		// Expected result in save
+		when(repository.save(any(MovieEntity.class))).thenReturn(movieEntity);
+
+		// Expected result in get reference by id
+		when(repository.getReferenceById(any(Long.class))).thenReturn(movieEntity);
+
+		// Expected result in delete by id
+		doNothing().when(repository).deleteById(any(Long.class));
+
+		// Expected result in existing by id
+		when(repository.existsById(1L)).thenReturn(true);
+	}
+
 	@Test
+	@DisplayName("Find All Movies Should Return Page Movie Data Transfer Object")
 	public void findAllShouldReturnPagedMovieDTO() {
 	}
 	
 	@Test
+	@DisplayName("Find By Id Movie Should Return Movie DTO Whe Id Exists")
 	public void findByIdShouldReturnMovieDTOWhenIdExists() {
 	}
 	
 	@Test
+	@DisplayName("Find By Id Movie Should Throw Resource Not Found Exception When Id Does Not Exists")
 	public void findByIdShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
 	}
 	
 	@Test
+	@DisplayName("Insert Movie Should Return Movie DTO")
 	public void insertShouldReturnMovieDTO() {
 	}
 	
 	@Test
+	@DisplayName("Update Movie Should Return Video DT When Id Exists")
 	public void updateShouldReturnMovieDTOWhenIdExists() {
 	}
 	
 	@Test
+	@DisplayName("Update Movie Should Throw Resource Not Found Exception When Id Does Not Exist")
 	public void updateShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
 	}
 	
 	@Test
+	@DisplayName("Delete Movie Should Do Nothing When Id Exists")
 	public void deleteShouldDoNothingWhenIdExists() {
 	}
 	
 	@Test
+	@DisplayName("Delete Movie Should Throw Resource Not Found Exception When Id Does Not Exist")
 	public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
 	}
 	
 	@Test
+	@DisplayName("Delete Movie Should Throw Database Exception When Dependent Id")
 	public void deleteShouldThrowDatabaseExceptionWhenDependentId() {
 	}
 }
